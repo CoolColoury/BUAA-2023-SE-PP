9838b0b75b9a7b5dc61e9aaf17f10a828204916e
LZN <1137480882@qq.com>
2023-03-08 21:19:42 +0800

    parse_words单元测试

---
+++ test/test_parser.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../src/parser.h"
+#include "../src/parser.cpp"
+#include "sstream"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace test
+{
+	TEST_CLASS(test_parser)
+	{
+	public:
+
+		TEST_METHOD(TestParseWordsFromCode)
+		{
+			Parser parser;
+			char input[] = "aa 1 aaa";
+			std::istringstream f(input);
+			parser.parse_words(f);
+			Assert::AreEqual(int(parser.get_words().size()), 2);
+			Assert::AreEqual(parser.get_words()[0].compare("aa"), 0);
+			Assert::AreEqual(parser.get_words()[1].compare("aaa"), 0);
+		}
+	};
+}
