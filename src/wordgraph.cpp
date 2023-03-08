b12069c1676ecc0b5a333af2494466c1967a208b
CoolColoury <1047813474@qq.com>
2023-03-08 22:16:22 +0800

    修改WordGraph实现

--- src/wordgraph.cpp
+++ src/wordgraph.cpp
@@ -6,14 +6,10 @@ WordGraph::WordGraph(std::vector<std::string> words)
     {
         m_word_graph.insert(make_pair(letter, std::vector<Edge>()));
     }
-    int edgeId = 0;
     for (std::string word : words)
     {
         // TODO释放edge，需要析构函数
-        Edge* edge = new Edge();
-        edge->id = edgeId++;
-        edge->word = word;
-        edge->length = int(word.size());
-        m_word_graph[tolower(word[0])].push_back(*edge);
+        Edge* edge = new Edge(edge_num++, word);
+        m_word_graph[edge->from].push_back(*edge);
     }
 }
--- src/wordgraph.h
+++ src/wordgraph.h
@@ -11,15 +11,26 @@ struct Edge
     int id;
     std::string word;
     int length;
+    char from;
+    char to;
+
+    Edge(int id, std::string word) :id(id), word(word)
+    {
+        length = int(word.size());
+        from = tolower(word.front());
+        to = tolower(word.back());
+    }
 };
 
 class WordGraph
 {
 private:
+    int edge_num = 0;
     std::map<char, std::vector<Edge>> m_word_graph;
 
 public:
     WordGraph(std::vector<std::string> words);
+    const std::vector<Edge>& get_edges(char node) const { return m_word_graph.at(node); }
 
 };
 
