d2df9f9bf929840a581632acce68d0c317fc5af8
LZN <1137480882@qq.com>
2023-03-08 21:19:16 +0800

    初步完成错误处理外的输入

--- src/parser.cpp
+++ src/parser.cpp
@@ -2,16 +2,102 @@
 
 void Parser::parse(int argc, char* argv[])
 {
+    check_args_error(argc, argv);
+    parse_arg(argc, argv);
+    std::ifstream f;
+    f.open(m_filename);
+    if (!f)
+    {
+        // TODO: Å×³öÒì³£
+    }
+    parse_words(f);
 }
 
 void Parser::parse_arg(int argc, char* argv[])
 {
+    for (int arg_i = 0; arg_i < argc; arg_i++) 
+    {
+        if (strcmp(argv[arg_i], "-n"))
+        {
+            m_config.type = 'n';
+            arg_i += 1;
+            m_filename = argv[arg_i];
+        }
+        else if (strcmp(argv[arg_i], "-w"))
+        {
+            m_config.type = 'w';
+            arg_i += 1;
+            m_filename = argv[arg_i];
+        }
+        else if (strcmp(argv[arg_i], "-c"))
+        {
+            m_config.type = 'c';
+            arg_i += 1;
+            m_filename = argv[arg_i];
+        }
+        else if (strcmp(argv[arg_i], "-h"))
+        {
+            arg_i += 1;
+            m_config.head = argv[arg_i][0];
+        }
+        else if (strcmp(argv[arg_i], "-t"))
+        {
+            arg_i += 1;
+            m_config.tail = argv[arg_i][0];
+        }
+        else if (strcmp(argv[arg_i], "-j"))
+        {
+            arg_i += 1;
+            m_config.n_head = argv[arg_i][0];
+        }
+        else if (strcmp(argv[arg_i], "-r"))
+        {
+            m_config.enable_loop = true;
+        }
+    }
 }
 
-void Parser::parse_words(const std::istream& input)
+void Parser::check_args_error(int argc, char* argv[])
 {
+    // TODO
 }
 
-void Parser::handle_error()
+void Parser::parse_words(std::istream& f)
 {
+    std::string buf;
+    stream_read_string_reverse(f, buf);
+
+    std::string word;
+    for (auto it = buf.begin(), end = buf.end(); it != end; ++it)
+    {
+        if (std::isalpha(*it))
+        {
+            word += *it;
+        }
+        else
+        {
+            if (!word.empty())
+            {
+                m_words.push_back(word);
+                word = std::string();
+            }
+        }
+    }
+    if (!word.empty())
+    {
+        m_words.push_back(word);
+    }
+}
+
+void stream_read_string_reverse(std::istream& f, std::string& result)
+{
+    f.seekg(0, std::istream::end);
+    std::streamoff len = f.tellg();
+    f.seekg(0);
+    if (len > 0)
+    {
+        result.reserve(static_cast<std::string::size_type>(len));
+    }
+    result.assign(std::istreambuf_iterator<char>(f.rdbuf()),
+        std::istreambuf_iterator<char>());
 }
--- src/parser.h
+++ src/parser.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <string>
 
 #ifndef _PARSER
 
@@ -22,13 +24,15 @@ class Parser
 private:
     std::vector<std::string> m_words;
     Config m_config;
+    char* m_filename;
 
     void parse_arg(int argc, char* argv[]);
-    void parse_words(const std::istream& input);
-    void handle_error(); // TODO
+    void check_args_error(int argc, char* argv[]);
 
 public:
     void parse(int argc, char* argv[]);
+    void parse_words(std::istream& f);
+
     std::vector<std::string>& get_words()
     {
         return m_words;
@@ -38,6 +42,9 @@ public:
         return m_config;
     }
 };
+
+// ¶ÁÈëº¯Êý
+void stream_read_string_reverse(std::istream& f, std::string& result);
 
 
 #endif // _PARSER
