91d2d5056e058dfeff6aeb51b8a7fe115f148253
CoolColoury <1047813474@qq.com>
2023-03-09 16:17:38 +0800

    私有化dfs_circle

--- src/wordgraph.h
+++ src/wordgraph.h
@@ -27,12 +27,12 @@ class WordGraph
 private:
     int edge_num = 0;
     std::map<int, std::vector<Edge>> m_word_graph;
+    bool dfs_circle(int now, std::vector<bool>& vis);
 
 public:
     WordGraph(const std::vector<std::string>& words);
     const std::vector<Edge>& get_edges(int node) const { return m_word_graph.at(node); }
     bool contain_circle();
-    bool dfs_circle(int now, std::vector<bool>& vis);
 };
 
 
